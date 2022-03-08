6293a96eea652a8411951387d9aa59bcc48da9b2
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2022-03-08 15:12:43 -0600

    Update

---
+++ Task1.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LogisticRegression
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+
+# Cargamos el conjunto de datos
+dataset = pd.read_csv('Walmart.csv')
+
+# Variables independientes: Weekly_Sales, Unemployment, Fuel_Price, CPI
+# Variables dependetientes: Date
+# Variable a predecir Weekly_Sales
+y = dataset.iloc[:, 2].values
+x = dataset.iloc[:, 0:1].values
+
+
+train_x, test_x, train_y, test_y = train_test_split(x,
+                                                    y,
+                                                    test_size=0.3)
+
+print(test_y)
+print(train_y)
+# Se define el grado del polinomio
+pol = PolynomialFeatures(degree=4)
+
+# Se transforma las caracteristicas
+x_train_poli = pol.fit_transform(train_x)
+x_test_poli = pol.fit_transform(test_x)
+
+pr = linear_model.LinearRegression()
+pr.fit(x_train_poli, train_y)
+Y_pred_pr = pr.predict(x_test_poli)
+
+plt.scatter(test_x, test_y)
+plt.plot(test_x, Y_pred_pr, color='red', linewidth=3)
+plt.show()
+
+print('DATOS DEL MODELO REGRESIÓN POLINOMIAL\n')
+
+print('Valor de la pendiente o coeficiente "a":\n')
+print(pr.coef_)
+print('Valor de la intersección o coeficiente "b":\n')
+print(pr.intercept_)
+print('Precisión del modelo:\n')
+print(pr.score(x_train_poli, train_y))
