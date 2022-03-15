e8c23bc949be375e3bc6419d076da7876adc93fe
coutino568 <40869460+coutino568@users.noreply.github.com>
2022-03-15 03:04:26 -0600

    Ordenados precision modelo : 0.74

--- Task1_cou.py
+++ Task1_cou.py
@@ -24,34 +24,31 @@ df2['Week'] = df2['Date'].dt.week
 #Variable a predecir: weekly sales
 
 
-# Variables independientes: Weekly_Sales, Unemployment, Fuel_Price, CPI
-# Variables dependetientes: Date
-# Variable a predecir Weekly_Sales
-y = df2.iloc[:, 2].values
-x = df2.iloc[:, 0:1].values
 
 
-train_x, test_x, train_y, test_y = train_test_split(x,
-                                                    y,
-                                                    test_size=0.3)
+
+indep = df2[['Store','Week','Temperature','Fuel_Price','Unemployment','Holiday_Flag','CPI']]
+#indep = df2[['Week']]
+dep= df2['Weekly_Sales']
 
 
 
 ## Selecciona filas aleatoriamente y las separa en entrenamiento y test
 
-indep = df2[['Store','Week','Temperature','Fuel_Price','Unemployment','Holiday_Flag','CPI']]
-#indep = df2[['Week','Temperature']]
-dep= df2['Weekly_Sales']
 
 X_training , X_testing, Y_training , Y_testing = train_test_split(indep,dep, test_size=0.3)
 X_training_df , X_testing_df = pd.DataFrame(X_training) , pd.DataFrame(X_testing)
 
 #print(Y_training)
-
 polynomial = PolynomialFeatures(degree=5)
 
-X_trainig_poly = polynomial.fit_transform(X_training_df)
-X_testing_poly = polynomial.fit_transform(X_testing_df)
+#primero es necesario ordenar para tener grafica legible 
+
+Y_training =Y_training.sort_index()
+Y_testing = Y_testing.sort_index()
+
+X_trainig_poly = polynomial.fit_transform(X_training_df.sort_index())
+X_testing_poly = polynomial.fit_transform(X_testing_df.sort_index())
 
 
 my_model = linear_model.LinearRegression()
@@ -62,33 +59,12 @@ my_intercept = my_model.intercept_
 
 
 
-
-#predicen resultados con el modelo hecho
-
-#generated_x = np.linspace(1,50)
-#generated_y = my_intercept + (generated_x*my_coeficients[1]) + (my_coeficients[2]*generated_x**2) + (my_coeficients[3]*generated_x**3)
-#print(X_testing)
-#print(Y_testing)
-
-
-#primero es necesario ordenar
-
-# sorted_indices = numpy.argsort(X_esting)
-# sorted_X = X_train[sorted_indices]
-# plt.plot(sorted_X, regressor.predict(sorted_X), color = 'blue')
-
-
-
-
-
-print(X_testing_poly)
 generated_y = my_model.predict(X_testing_poly)
 
 # graficamos la informacion del tes y la ifnormacion del test predicha
 plt.scatter(X_testing['Week'], Y_testing, s=3)
 plt.plot(X_testing_poly,generated_y,linewidth=1, color='g')
 plt.show()
-
 
 
 print('Precisión del modelo:\n')
